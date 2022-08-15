5d453268968d9c364067540654f493892b0dd2b8
Theo Lemaire <theo.lemaire1@gmail.com>
2022-08-15 10:38:40 -0400

    new msg

--- lab_instruments/sutter_mp285a.py
+++ lab_instruments/sutter_mp285a.py
@@ -2,7 +2,7 @@
 # @Author: Theo Lemaire
 # @Date:   2022-04-27 18:16:34
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2022-08-15 09:59:10
+# @Last Modified time: 2022-08-15 10:38:30
 
 import serial
 import struct
@@ -81,6 +81,7 @@ class SutterMP285A:
 
         :param timeout: commands timeout (s)
         '''
+        print('hello')
         self.connect()
         self.timeout = timeout
         self.set_absolute_mode()
