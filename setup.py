f9d9dc41f27c2b6ba4ef5543a8158f733fb507ea
Theo Lemaire <theo.lemaire1@gmail.com>
2024-05-08 19:02:39 -0400

    version increment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2017-06-13 09:40:02
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2023-08-09 17:12:23
+# @Last Modified time: 2024-05-08 19:02:30
 
 import os
 from setuptools import setup
@@ -25,7 +25,7 @@ def getFiles(path):
 
 setup(
     name='instrulink',
-    version='1.0.3',
+    version='1.0.4',
     description='Python package to interface diverse laboratory instruments',
     long_description_content_type='text/markdown',
     long_description=readme(),
